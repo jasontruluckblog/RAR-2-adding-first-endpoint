2f807d1cf1a8a2cbcc2354db2959a9faa77b9683
Jason Truluck <jason.truluck@gmail.com>
2014-04-11 14:42:16 -0400

    Update a comments to make the source more clear

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ $:.unshift File.dirname(__FILE__)
 
 require 'boot'
 
+# Setup configuration settings
 module Application
   include ActiveSupport::Configurable
 end
@@ -17,6 +18,7 @@ Bundler.require :default, Application.config.env
 specific_environment = "../environments/#{Application.config.env}.rb"
 require specific_environment if File.exists? specific_environment
 
+# simple method to require files by path
 def include_files(path)
   files = "#{Application.config.root}#{path}/*.rb"
   Dir[files].each {|f| require f}
@@ -25,4 +27,5 @@ end
 # Require public API files
 include_files("/app/api")
 
+# Require the base api file
 require File.expand_path('../../app/api.rb', __FILE__)
